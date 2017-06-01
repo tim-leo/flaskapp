c9f01751760a703e18bd97eed223872b671b96a9
timleo <timleo@byu.edu>
2017-06-01 16:31:50 -0500

    added a new index

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "eat a dick"
-
-
+    user = {'nickname': 'big'}
+    return render_template('index.html', title='Home', user=user)
