09afd8c9d7a5245053ca2506921e224043e21380
timleo <timleo@byu.edu>
2017-06-08 23:39:42 -0500

    changed the init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,30 @@
 from flask import Flask
 
 app = Flask(__name__)
+@app.route('/')
+@app.route('/index')
+def index():
+    """Creates an array with random names."""
+    user = {'nickname': 'big'}
+
+    posts = []
+
+    i = 1
+    while i < 20:
+        posts.append({
+            'author': {'nickname': 'Person' + str(i)},
+            'body': 'some message about weenies' + str(i)
+        })
+        i += 1
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/validusers')
+def validusers():
+    """Creates an array with random names."""
+
+    return render_template('validusers.html', title='Users')
+
 app.config.from_object('config')
 if __name__ == '__main__':
     app.run()
