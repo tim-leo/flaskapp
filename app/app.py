ee1754b2c8bd0af21a9b549f568974104f26129a
timleo <timleo@byu.edu>
2017-06-09 11:09:19 -0500

    fuckyeah changed app again

--- app/app.py
+++ app/app.py
@@ -32,6 +32,34 @@
 #
 # from app import views
 
-from flask import Flask
+from flask import Flask, render_template
+from app import app
+import json
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    """Creates an array with random names."""
+    user = {'nickname': 'big'}
+
+    posts = []
+
+    i = 1
+    while i < 20:
+        posts.append({
+            'author': {'nickname': 'Person' + str(i)},
+            'body': 'some message about weenies' + str(i)
+        })
+        i += 1
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/validusers')
+def validusers():
+    """Creates an array with random names."""
+
+    return render_template('validusers.html', title='Users')
+
 
 app = Flask(__name__)
